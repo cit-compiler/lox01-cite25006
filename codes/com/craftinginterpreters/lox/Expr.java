28384842925402cfe1340f30a8690ac436930138
cite25006 <cite25006@g.nihon-u.ac.jp>
2025-12-09 12:15:16 +0900

    fix Expr.java, generated GenerateAst.java

--- codes/com/craftinginterpreters/lox/Expr.java
+++ codes/com/craftinginterpreters/lox/Expr.java
@@ -1,17 +1,24 @@
 package com.craftinginterpreters.lox;
 
-abstract class Expr { 
+abstract class Expr {
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    // ...
+  }
+
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
-      this.left = left;
-      this.operator = operator;
-      this.right = right;
+      // ...
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
     }
 
     final Expr left;
-    final Token operator;
-    final Expr right;
+    // ...
   }
 
-  // Other expressions...
+  abstract <R> R accept(Visitor<R> visitor);
 }
--- codes/com/craftinginterpreters/tool/GenerateAst.java
+++ codes/com/craftinginterpreters/tool/GenerateAst.java
@@ -37,9 +37,26 @@ public class GenerateAst {
         String fields = type.split(":")[1].trim(); 
         defineType(writer, baseName, className, fields);
       }
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
     writer.println("}");
     writer.close();
+    
+    defineVisitor(writer, baseName, types);
+
   }
+    private static void defineVisitor(
+        PrintWriter writer, String baseName, List<String> types) {
+      writer.println("  interface Visitor<R> {");
+
+      for (String type : types) {
+        String typeName = type.split(":")[0].trim();
+        writer.println("    R visit" + typeName + baseName + "(" +
+            typeName + " " + baseName.toLowerCase() + ");");
+      }
+
+      writer.println("  }");
+    }
     private static void defineType(
         PrintWriter writer, String baseName,
         String className, String fieldList) {
@@ -56,6 +73,13 @@ public class GenerateAst {
         writer.println("      this." + name + " = " + name + ";");
       }
 
+      writer.println("    }");
+
+      writer.println();
+      writer.println("    @Override");
+      writer.println("    <R> R accept(Visitor<R> visitor) {");
+      writer.println("      return visitor.visit" +
+          className + baseName + "(this);");
       writer.println("    }");
 
       // Fields.
